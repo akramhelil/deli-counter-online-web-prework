7da57b2eb514bcaa277138fb6df0a1a9bc487d7e
Akram Helil <akramhelil@gmail.com>
2019-01-14 16:31:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ end
 
 def now_serving(deli)
   if deli.empty?
-    puts "The line is empty."
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving#{deli.first}"
     deli.first
