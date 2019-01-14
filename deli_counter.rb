ef19dfa45f22047aeabee356624784c841845a12
Akram Helil <akramhelil@gmail.com>
2019-01-14 16:27:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 katz_deli = []
-def line (katz_deli)
-if katz_deli.empty?
+def line (deli)
+if deli.empty?
   puts "The line is currently empty."
 else
   current_line = "The line is currently:"
@@ -10,4 +10,9 @@ else
    end
    puts current_line
  end
+end
+
+def take_a_number(deli, name)
+  deli<<name
+puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
